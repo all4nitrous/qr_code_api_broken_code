9232f88261bf07392e54c1a989f5d1a548ccc04e
all4nitrous <all4nitrous@gmail.com>
2024-11-19 23:32:01 -0500

    more changes

--- app/main.py
+++ app/main.py
@@ -1,14 +1,29 @@
 from fastapi import FastAPI
 from app.config import QR_DIRECTORY
 from app.routers import qr_code, oauth  # Make sure these imports match your project structure.
-from app.services.qr_service import create_directory
 from app.utils.common import setup_logging
+import logging
+import os
+
+
+# Function to create a directory and handle permission issues
+def create_directory(directory_path):
+    try:
+        os.makedirs(directory_path, exist_ok=True)  # Create the directory if it doesn't exist
+        logging.info(f"Directory created or already exists: {directory_path}")
+    except PermissionError as e:
+        logging.error(f"Permission denied when trying to create directory {directory_path}: {e}")
+        raise
+    except Exception as e:
+        logging.error(f"An unexpected error occurred while creating directory {directory_path}: {e}")
+        raise
+
 
 # This function sets up logging based on the configuration specified in your logging configuration file.
 # It's important for monitoring and debugging.
 setup_logging()
 
-# This ensures that the directory for storing QR codes exists when the application starts.
+# Ensure the directory for storing QR codes exists when the application starts.
 # If it doesn't exist, it will be created.
 create_directory(QR_DIRECTORY)
 
@@ -18,7 +33,7 @@ app = FastAPI(
     description="A FastAPI application for creating, listing available codes, and deleting QR codes. "
                 "It also supports OAuth for secure access.",
     version="0.0.1",
-        redoc_url=None,
+    redoc_url=None,
     contact={
         "name": "API Support",
         "url": "http://www.example.com/support",
@@ -28,7 +43,6 @@ app = FastAPI(
         "name": "Apache 2.0",
         "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
     }
-
 )
 
 # Here, we include the routers for our application. Routers define the paths and operations your API provides.
